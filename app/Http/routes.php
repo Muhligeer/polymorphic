9d77892f7b39c3d9667b0a7cca5d391c8cde027e
Muhligeer <guilhermejosecruz@gmail.com>
2019-03-08 10:32:43 -0300

    08/03

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,68 @@
 |
 */
 
+use App\Photo;
+use App\Staff;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/create', function(){
+
+    $staff = Staff::find(1);
+
+    $staff->photos()->create(['path'=>'example.jpg']);
+
+});
+
+Route::get('/read', function(){
+
+    $staff = Staff::findOrFail(1);
+
+    foreach($staff->photos as $photo){
+
+        echo $photo->path;
+
+    }
+
+});
+
+Route::get('/update', function(){
+
+    $staff = Staff::findOrFail(1);
+
+    $photo = $staff->photos()->whereId(1)->first();
+
+    $photo->path = "updated.jpg";
+
+    $photo->save();
+
+});
+
+Route::get('/delete', function(){
+
+    $staff = Staff::findOrFail(1);
+
+    $staff->photos()->delete();
+
+});
+
+Route::get('/assign', function (){
+
+    $staff = Staff::findOrFail(1);
+
+    $photo = Photo::findOrFail(4);
+
+    $staff->photos()->save($photo);
+
+});
+
+//Route::get('/un-assign', function (){
+//
+//    $staff = Staff::findOrFail(1);
+//
+//    $staff->photos()->whereId(4)->update(['imageable_id'=>'', 'imageabe_type'=>'']);
+//
+//});
